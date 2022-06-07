1688bc858d6c4a84665e8e0e456668640c186901
BlueStixs <100892247+BlueStixs@users.noreply.github.com>
2022-06-07 18:54:47 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,47 @@
+var i = 0;
+var j = 0;
+
+var randomNum = Math.floor(Math.random() * (list.length - 1));
+
+function randomize(word) {
+    if (word == "startingWord"){
+        document.getElementById("endingWord").innerHTML = "Ending Word";
+    }
+
+    disableBtns();
+    if (i < 3) {
+        setTimeout(function () {
+            document.getElementById(word).innerHTML = list[j];
+            j++;
+            if (j < list.length) {
+                randomize(word);
+            } else {
+                i++;
+                j = 0;
+                randomize(word);
+            }
+        }, 50);
+    } else {
+        var randomWord = list[randomNum];
+        document.getElementById(word).innerHTML = randomWord;
+
+        //reset
+        i = 0;
+        j = 0;
+        randomNum = Math.floor(Math.random() * (list.length - 1));
+        enableBtns();
+
+        document.getElementById(word).innerHTML = randomWord;
+        document.getElementById(word).setAttribute("href", "https://genshin-impact.fandom.com/wiki/" + randomWord);
+    }
+}
+
+function disableBtns() {
+    document.getElementById("startBtn").disabled = true;
+    document.getElementById("endBtn").disabled = true;
+}
+
+function enableBtns() {
+    document.getElementById("startBtn").disabled = false;
+    document.getElementById("endBtn").disabled = false;
+}
